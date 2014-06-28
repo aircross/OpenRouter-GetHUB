4d95227bdcc2eb93d4c7ef7b7d17edcc35406e4b
live-8 <empire.x@qq.com>
2014-06-28 18:01:00 +0800

    201406281800

--- luci/applications/luci-database/luasrc/model/cbi/database.lua
+++ luci/applications/luci-database/luasrc/model/cbi/database.lua
@@ -5,7 +5,15 @@
 local fs = require "nixio.fs"
 local util = require "nixio.util"
 
-m = Map("database", translate("database"))
+local nginx_running=(luci.sys.call("pidof nginx > /dev/null") == 0)
+local php_running=(luci.sys.call("pidof php-cgi > /dev/null") == 0)
+local mysqld_running=(luci.sys.call("pidof mysqld > /dev/null") == 0)
+
+if nginx_running and php_running and mysqld_running then
+	m = Map("database", translate("database"), translate("database is running..."))
+else
+	m = Map("database", translate("database"), translate("database is stop!"))
+end
 
 --挂载点
 local rst=0
@@ -17,7 +25,7 @@ s.anonymous = true
 
 --初始化
 if(rst == 0)then
-	enable = s:option(Flag, "enable", translate("start init database"))
+	enable = s:option(DummyValue, "")
 	enable.rmempty = false
 elseif(rst == 1)then
 	enable = s:option(DummyValue, "", translate("硬盘未插,无法启动服务！"))
@@ -31,6 +39,12 @@ elseif(rst == 3)then
 	enable.rmempty = false
 end
 
+select=s:option(ListValue,"select",translate("select action"))
+select:value("init","database init")
+select:value("start","start server")
+select:value("stop","stop server")
+select:value("del","delete database")
+
 local devices = {}
 util.consume((fs.glob("/dev/sd??*")), devices)
 
@@ -40,17 +54,27 @@ for i, dev in ipairs(devices) do
 end
 
 --用户名
-usr_name = s:option(DummyValue, "", translate("user name is: &nbsp; &nbsp; root"))
+--usr_name = s:option(DummyValue, "root", translate("user name is: root"))
+--usr_name:depends("select","init")
 
 --密码
-passwd = s:option(Value, "passwd", translate("user passwd"), translate("if passwd is empty, default passwd is: openrouter"))
+passwd = s:option(Value, "passwd", translate("user passwd"), translate("default user name: root; if passwd is empty, default passwd is: openrouter"))
+passwd:depends("select","init")
 passwd.password = true
 
+--数据库名字
 database_name = s:option(Value, "database_name", translate("database name"), translate("if database name is empty, default database name is: openrouter"))
+database_name:depends("select","init")
 
---服务开始
-server_start = s:option(Flag, "server_start", translate("server start"))
-server_start:depends("enable", 1)
+--php port
+nginx_port = s:option(Value, "nginx_port", translate("openrouter port"))
+nginx_port:depends("select","start")
+nginx_port.default = 90
+
+--web port
+uhttpd_port = s:option(Value, "uhttpd_port", translate("web server port"))
+uhttpd_port:depends("select","start")
+uhttpd_port.default = 80
 
 return m
 
